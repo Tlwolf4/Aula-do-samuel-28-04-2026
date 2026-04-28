1df174b3bb88567aca30c036c575b31aee857a92
Eryck fernando <eryck.barbosa@aluno.ce.gov.br>
2026-04-28 20:01:33 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,17 +1,1 @@
-import Doitoiévski from "./conponets/Doitoevisk"
 
-export default function App() {
-    return(
-        <div>
-            <marquee direction="left">
-                <h1> Oi meu mano, blz
-
-                 Do que tal ler essa linda obra de Doitoiévski!!   
-                </h1>
-            </marquee>
-
-            <Doitoiévski/>
-        </div>       
-    
-    )
-}
