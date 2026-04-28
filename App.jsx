4e95a78b44bf8c45b36e68353126282ec0409d57
Eryck fernando <eryck.barbosa@aluno.ce.gov.br>
2026-04-28 19:59:47 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Doitoiévski from "./conponets/Doitoevisk"
+
+export default function App() {
+    return(
+        <div>
+            <marquee direction="left">
+                <h1> Oi meu mano, blz
+
+                 Do que tal ler essa linda obra de Doitoiévski!!   
+                </h1>
+            </marquee>
+
+            <Doitoiévski/>
+        </div>       
+    
+    )
+}
